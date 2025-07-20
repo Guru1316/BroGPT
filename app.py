98afe5e8e7550c87012e67142b8f1c4572549032
Guru1316 <guruprasadgdr1@gmail.com>
2025-07-21 00:15:33 +0530

    fix: set host and port for Render

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from logic.groq_client import ask_groq
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -9,9 +13,10 @@ def index():
 
 @app.route("/ask", methods=["POST"])
 def ask():
-    user_input = request.json["message"]
+    user_input = request.json.get("message")
     response = ask_groq(user_input)
     return jsonify({"reply": response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render uses dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
